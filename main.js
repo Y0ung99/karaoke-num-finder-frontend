29e5227b0ab82a222b438b27fcda67e5aa3ba999
Y0ung99 <ancolor@knou.ac.kr>
2022-08-19 05:30:17 +0900

    검색후 검색결과만큼의 버튼들이 나오도록 구현

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const searchTab = document.querySelector('.searchTab');
 const popularTab = document.querySelector('.popularTab');
 const newsongTab = document.querySelector('.newsongTab');
 const bookmarkTab = document.querySelector('.bookmarkTab');
+const anchors = document.querySelector('.anchors');
 
 const selectDate = document.querySelector('#selectDate');
 const selectCompany = document.querySelector('#selectCompany');
@@ -29,8 +30,10 @@ bookmarkTab.addEventListener('click', () => {
 searchBtn.addEventListener('click', () => {
     search()
     .then(songs => {
-        console.log(songs);
+        const anchor = songs.pop();
         list.innerHTML = songs.map(song => listHTML(song)).join('');
+        anchors.innerHTML = createAnchors(anchor.page);
+        linkAnchors(anchor.page);
     })
 
     
@@ -60,11 +63,11 @@ function hideSearchBar() {
     searchBtn.style.visibility = 'hidden';
 }
 
-async function search() { // 
+async function search(page = 1) { // 
     const text = searchTxt.value;
     const company = selectCompany.value;
     const option = selectOptions.value;
-    const data = {text, company, option};
+    const data = {text, company, option, page};
 
     const response = await fetch('http://127.0.0.1:8080/search', {
         method: 'POST',
@@ -72,7 +75,7 @@ async function search() { //
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data),
-    })
+    });
 
     return response.json();
 }
@@ -91,4 +94,27 @@ function listHTML(song) {
     </div>
     <div class="divider"></div>  
 `
+}
+
+function createAnchors(numPage) {
+    let html = [];
+    for (let i = 1; i < numPage + 1; i++) {
+        html.push(`<li><button class="a${i}">${i}</button></li>`);
+    }
+    return html.join('');
+}
+
+function linkAnchors(numPage) {
+    for (let i = 1; i < numPage + 1; i++) {
+        const a = document.querySelector(`.a${i}`);
+        a.addEventListener('click', (event) => {
+            search(i)
+            .then(songs => {
+                const anchor = songs.pop();
+                list.innerHTML = songs.map(song => listHTML(song)).join('');
+                anchors.innerHTML = createAnchors(anchor.page);
+                linkAnchors(anchor.page);
+            })
+        })
+    }
 }
